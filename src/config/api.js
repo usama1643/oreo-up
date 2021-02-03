b5311dcb253fa624d687951fd3f6a6c4939ad2de
Muhammad Usama <muhammadusama1643@gmail.com>
2021-02-03 14:39:35 +0500

    updated oreo

---
+++ src/config/api.js
@@ -0,0 +1,10 @@
+export const API = 'https://generations.com.pk';
+
+export const CONSUMER_KEY = 'ck_db854e045f7b18dacea4f26183e9edaaa4af9c86';
+export const CONSUMER_SECRET = 'cs_965691c9413bd1b7d5da6b4e650d23ea3a3230a0';
+
+export default {
+  API_ENDPOINT: API,
+  CONSUMER_KEY,
+  CONSUMER_SECRET,
+};
